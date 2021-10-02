6dbc795a0d6d249ee1e86250228e59bee357386c
Patryk Kozyczkowski <patrykkozyczkowski@gmail.com>
2021-10-02 14:37:33 +0200

    ...

--- run.py
+++ run.py
@@ -1,6 +1,14 @@
+import os
+
 from db import db
 from app import app
 
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///data.db")
+if DATABASE_URL.startswith("postgres://"):
+    uri = DATABASE_URL.replace("postgres://", "postgresql://", 1)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URL
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db.init_app(app)
 
 
